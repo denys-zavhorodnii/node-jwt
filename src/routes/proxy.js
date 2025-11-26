57e0749c323770514a24c27f599382b1ecca738b
Denys Zavhorodnii <denis.zavgorodny@gmail.com>
2025-11-27 01:52:20 +0200

    simplify

--- src/routes/proxy.js
+++ src/routes/proxy.js
@@ -6,19 +6,6 @@ export function addApiRoute() {
     const API_KEY = "some key";
 
     fastify.get('/api', async (req, reply) => {
-        const authHeader = req.headers.authorization;
-
-
-        if (!authHeader) {
-            reply.headers({
-                'WWW-Authenticate': 'Bearer',
-            })
-            reply.code(401)
-            return reply.send({
-                code: 401,
-            })
-        }
-
         const cats = await getCats(fetch, API_KEY)
 
         reply.type('application/json')
